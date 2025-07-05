5af29d6302fd1dc5f7dcb67352a9f9e4b50936ec
Al-Amin <alamin.parliament.bd@gmail.com>
2025-07-05 21:51:44 +0600

    Updated v17 05/07/2025

--- bps_inventory/settings.py
+++ bps_inventory/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
         'USER': config('DB_USER', default='root'),
         'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST': config('DB_HOST', default='127.0.0.1'),
-        'PORT': config('DB_PORT', default='3306'),
+        'PORT': config('DB_PORT', default='3307'),
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
